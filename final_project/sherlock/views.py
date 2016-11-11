33586ee7f46621ff7ee9bca80780888ce924f3db
tdhuynh <tommyhuynh93@gmail.com>
2016-11-11 12:09:35 -0500

    created views for index, profile, usercreate. templates for base and index

--- final_project/sherlock/views.py
+++ final_project/sherlock/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm, User
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from sherlock.models import Profile, About
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy("index_view")
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
+class ProfileView(ListView):
+    model = Profile
