76c0b4b9ad564b4d08d2fed9adfb7aab33229a99
destanton <delisestanton@gmail.com>
2016-11-11 23:18:13 -0500

    added vertical menu, profile update, html changes

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from sherlock.views import IndexView, UserCreateView, ProfileDetailView
+from sherlock.views import IndexView, UserCreateView, ProfileDetailView, ProfileUpdateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', IndexView.as_view(), name="index_view"),
     url(r'^accounts/profile/$', ProfileDetailView.as_view(), name="profile_detail_view"),
+    url(r'^update/(?P<pk>\d+)/$', ProfileUpdateView.as_view(), name='profile_update_view'),
     url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- final_project/sherlock/views.py
+++ final_project/sherlock/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.forms import UserCreationForm, User
 from django.urls import reverse_lazy, reverse
 
@@ -21,4 +21,13 @@ class ProfileDetailView(DetailView):
     model = Profile
 
     def get_object(self):
+        return Profile.objects.get(user=self.request.user)
+
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    success_url = reverse_lazy('profile_detail_view')
+    fields = ('picture', 'first_name', 'middle_name', 'last_name', 'gender', )
+
+    def get_objects(self):
         return Profile.objects.get(user=self.request.user)
