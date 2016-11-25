1839dbcd5be5ecc6ba7b1227e26ecdfc52b950e1
destanton <delisestanton@gmail.com>
2016-11-25 13:52:55 -0500

    added name field to contact submission

--- sherlock/forms.py
+++ sherlock/forms.py
@@ -4,18 +4,21 @@ from haystack.forms import SearchForm
 
 
 class ContactForm(forms.Form):
-    sender = forms.CharField(max_length=50)
+    name = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=50)
     message = forms.CharField(widget=forms.Textarea)
 
     def send_email(self):
-        sender = self.cleaned_data["sender"]
+        name = self.cleaned_data["name"]
+        email = self.cleaned_data["email"]
         message = self.cleaned_data["message"]
-        subject = "Contact Form Submission!"
+        subject = "Contact Submission Form!"
         body = """
         Hey Guys! You got a new contact form submission:
         From: {}
+        At: {}
         Message: {}
-        """.format(sender, message)
+        """.format(name, email, message)
         recipient_list = ["tdhdesapp@gmail.com"]
         send_mail(subject, body, 'Do Not Reply <do_not_replay@domain.com>', recipient_list)
 
