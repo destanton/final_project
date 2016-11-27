7274cd6ccfb8d213e6d4b01925cfc8143daa0800
tdhuynh <tommyhuynh93@gmail.com>
2016-11-27 16:51:23 -0500

    working search index

--- sherlock/search_indexes.py
+++ sherlock/search_indexes.py
@@ -4,20 +4,20 @@ from haystack.utils import Highlighter
 
 
 
-class ProfileIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.EdgeNgramField(document=True, use_template=True)
-    # suggestions = indexes.FacetCharField()
-    #
-    # def prepare(self, obj):
-    #     prepared_data = super(ProfileIndex, self).prepare(obj)
-    #     prepared_data["suggestions"] = prepared_data["text"]
-    #     return prepared_data
-
-    def get_model(self):
-        return Profile
-
-    def index_queryset(self, using=None):
-        return Profile.objects.all()
+# class ProfileIndex(indexes.SearchIndex, indexes.Indexable):
+#     text = indexes.EdgeNgramField(document=True, use_template=True)
+#     # suggestions = indexes.FacetCharField()
+#     #
+#     # def prepare(self, obj):
+#     #     prepared_data = super(ProfileIndex, self).prepare(obj)
+#     #     prepared_data["suggestions"] = prepared_data["text"]
+#     #     return prepared_data
+#
+#     def get_model(self):
+#         return Profile
+#
+#     def index_queryset(self, using=None):
+#         return Profile.objects.all()
 
 
 class AboutIndex(indexes.SearchIndex, indexes.Indexable):
