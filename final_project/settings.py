8f8cc9ce612a5b33718c859ed1ff4310484e343e
tdhuynh <tommyhuynh93@gmail.com>
2016-11-27 13:05:23 -0500

    changed url parse module import

--- final_project/settings.py
+++ final_project/settings.py
@@ -1,7 +1,7 @@
 
 import os
 from django.urls import reverse_lazy
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
