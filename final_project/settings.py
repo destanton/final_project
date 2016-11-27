7c1b8119ba83856507dfa5e1ba1f44f255f90a6c
tdhuynh <tommyhuynh93@gmail.com>
2016-11-27 17:55:49 -0500

    commented out static url and file storage

--- final_project/settings.py
+++ final_project/settings.py
@@ -170,5 +170,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 if AWS_STORAGE_BUCKET_NAME:
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
     AWS_S3_FILE_OVERWRITE = False
-    STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    # STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+    # STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
