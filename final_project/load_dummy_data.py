422b4ebfb12d6430d86447cdc5bf83849dff5bb9
tdhuynh <tommyhuynh93@gmail.com>
2016-11-29 21:59:47 -0500

    cleaning up data in abouts

--- final_project/load_dummy_data.py
+++ final_project/load_dummy_data.py
@@ -9,26 +9,42 @@ hospitals = ['John Doe Memorial Hospital', 'St. Saint Hospital',
              'Johns Hopkins Hospital', 'General Hospital',
              'Sacred Heart Hospital', 'Duke Hospital']
 
-def create_data():
-    pic_links = []
-    url = "https://randomuser.me/api/?nat=us&results=10"
-    results = requests.get(url).json()
-    all_res = results["results"]
-    for res in all_res:
-        x = User.objects.create_user(username=res['login']['username'], password="safepass")
-        Profile.objects.filter(user=x).update(first_name=res['name']['first'], last_name=res['name']['last'], gender=res['gender'])
-        About.objects.filter(user=x).update(birthdate=res['dob'][:10], city_of_birth=res['location']['city'], state_of_birth=res['location']['state'], birth_hospital=choice(hospitals))
-        prof = Profile.objects.get(user=x)
-        about = About.objects.get(user=x)
-        pic_links.append(res['picture']['large'])
 
+# def create_data():
+#     pic_links = []
+#     url = "https://randomuser.me/api/?nat=us&results=10"
+#     results = requests.get(url).json()
+#     all_res = results["results"]
+#     for res in all_res:
+#         x = User.objects.create_user(username=res['login']['username'], password="safepass")
+#         Profile.objects.filter(user=x).update(first_name=res['name']['first'], last_name=res['name']['last'], gender=res['gender'])
+#         About.objects.filter(user=x).update(birthdate=res['dob'][:10], city_of_birth=res['location']['city'], state_of_birth=res['location']['state'], birth_hospital=choice(hospitals))
+#         prof = Profile.objects.get(user=x)
+#         about = About.objects.get(user=x)
+#         pic_links.append(res['picture']['large'])
 
-['https://randomuser.me/api/portraits/men/16.jpg', 'https://randomuser.me/api/portraits/men/98.jpg', 'https://randomuser.me/api/portraits/men
-/62.jpg', 'https://randomuser.me/api/portraits/women/77.jpg', 'https://randomuser.me/api/portraits/women/10.jpg', 'https://randomuser.me/api/
-portraits/men/8.jpg', 'https://randomuser.me/api/portraits/men/97.jpg', 'https://randomuser.me/api/portraits/women/78.jpg', 'https://randomus
-er.me/api/portraits/men/2.jpg', 'https://randomuser.me/api/portraits/men/34.jpg']
+EYE_COLOR = [
+    'Black',
+    'Blue',
+    'Brown',
+    'Gray',
+    'Green',
+    'Hazel',
+    'Other'
+]
 
-['https://randomuser.me/api/portraits/women/26.jpg', 'https://randomuser.me/api/portraits/women/65.jpg', 'https://randomuser.me/api/portraits
-/men/13.jpg', 'https://randomuser.me/api/portraits/men/57.jpg', 'https://randomuser.me/api/portraits/men/32.jpg', 'https://randomuser.me/api/
-portraits/women/38.jpg', 'https://randomuser.me/api/portraits/men/27.jpg', 'https://randomuser.me/api/portraits/women/26.jpg', 'https://rando
-muser.me/api/portraits/men/75.jpg', 'https://randomuser.me/api/portraits/women/59.jpg']
+FAMILY = ['Cousin',
+    'Child',
+    'Parent',
+    'Sibling',
+    'Aunt',
+    'Uncle',
+    'Grandparent'
+]
+
+about = User.objects.all()
+
+for x in about:
+    About.objects.filter(user=x).update(eye_color=choice(EYE_COLOR), searching_for=choice(FAMILY))
+    y = About.objects.get(user=x)
+    print(y.eye_color, y.searching_for)
