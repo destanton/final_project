1d831fa8e8cbbf4d2e3ae8356bb08ea525d9c8bb
tdhuynh <tommyhuynh93@gmail.com>
2017-01-08 17:09:10 -0500

    edit settings and picture

--- final_project/settings.py
+++ final_project/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'final_project.urls'
